cab939a5ba921a0f6d3f95cd2e7344d14607248d
Stanislav Khalash <stanislav.khalash@gmail.com>
2018-02-06 23:15:08 +0100

    Initial commit

---
+++ RxSandbox/RxSandbox.Tests/ColdHotObservablesFixture.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Nito.AsyncEx;
+
+namespace RxSandbox.Tests
+{
+    [TestFixture]
+    public class ColdHotObservablesFixture
+    {
+        [Test]
+        public async Task Interval_IsColdObservable()
+        {
+            var firstSubscription = new TaskCompletionSource();
+            var secondSubscription = new TaskCompletionSource();
+
+            var observable = Observable.Interval(TimeSpan.FromSeconds(1)).Take(5);
+
+            observable
+                .Subscribe(i => TestContext.Out.WriteLine($"First subscription --> {i}"), 
+                () => firstSubscription.SetResult());
+
+            await Task.Delay(TimeSpan.FromSeconds(3));
+
+            observable
+                .Subscribe(i => TestContext.Out.WriteLine($"Second subscription --> {i}"),
+                () => secondSubscription.SetResult());
+
+            await Task.WhenAll(firstSubscription.Task, secondSubscription.Task);
+        }
+
+        [Test]
+        public async Task Interval_WhenPublishedAndConnected_IsColdObservable()
+        {
+            var firstSubscription = new TaskCompletionSource();
+            var secondSubscription = new TaskCompletionSource();
+
+            var observable = Observable.Interval(TimeSpan.FromSeconds(1)).Take(5).Publish();
+            observable.Connect();
+
+            observable
+                .Subscribe(i => TestContext.Out.WriteLine($"First subscription --> {i}"),
+                () => firstSubscription.SetResult());
+
+            await Task.Delay(TimeSpan.FromSeconds(3));
+
+            observable
+                .Subscribe(i => TestContext.Out.WriteLine($"Second subscription --> {i}"),
+                () => secondSubscription.SetResult());
+
+            await Task.WhenAll(firstSubscription.Task, secondSubscription.Task);
+        }
+    }
+}
